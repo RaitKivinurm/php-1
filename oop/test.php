5f0665a2ac2f47e9e86b49f054fffae0fc4714ec
AnnaKarutina <anna.karutina@khk.ee>
2017-01-11 15:29:03 +0200

    Create and use color text object

--- oop/test.php
+++ oop/test.php
@@ -5,8 +5,9 @@
  * Date: 11.01.2017
  * Time: 14:46
  */
-// require the object creating and using class
+// require the object creating and using classes
 require_once('text.php');
+require_once 'ctext.php';
 // create an object
 $sentence = new text();
 // control object output
@@ -30,5 +31,20 @@ print_r($sentence2);
 echo '</pre>';
 // show object output
 $sentence2->show();
-
+echo '<hr/>';
+// create an object
+$sentence3 = new ctext('Hello color text by construct!');
+// control object output
+echo '<pre>';
+print_r($sentence3);
+echo '</pre>';
+// show object output
+$sentence3->show();
+// set object color
+$sentence3->setColor('#FF0000');
+echo '<pre>';
+print_r($sentence3);
+echo '</pre>';
+// show object output
+$sentence3->show();
 ?>
