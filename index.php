186b1e1ac4bbd89606c72b1146147683e8f2fe4d
anna.karutina <anna.karutina@khk.ee>
2017-01-18 09:34:14 +0200

    Control linkobject pair creation

--- index.php
+++ index.php
@@ -55,4 +55,8 @@ $http->set('tund', 'php programmeerimisvahendid');
 echo '<pre>';
 print_r($http->vars);
 echo '</pre>';
+// control linkobject data pair creation
+$link = ''; // empty link for data pairs
+$http->addToLink($link, 'kasutaja', 'anna');
+$http->addToLink($link, 'parool', 'qwerty');
 ?>
