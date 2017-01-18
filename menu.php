9a63f6e53217dfb776ff188cb6c0f144be0a1516
anna.karutina <anna.karutina@khk.ee>
2017-01-18 10:44:40 +0200

    Add file path replasement dir.file to dir/file

---
+++ menu.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 18.01.2017
+ * Time: 10:33
+ */
+// menu.php - create page menu
+// create menu template objects
+// for menu and menu itmes
+$menu = new template('menu.menu');
